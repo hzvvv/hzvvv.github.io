6d3db51d5fac94ff856b3e54bf1cb0a54dfda522
学长 <36513711+hzvvv@users.noreply.github.com>
2023-05-05 16:27:31 +0800

    Save index1.js.

--- index1.js
+++ index1.js
@@ -13,15 +13,15 @@ var url_No1 = "https://hzvvv.github.io/tool";
 var url = window.location.pathname;
 if (url == "/nets-tmr-pac-pad/viewCustomerVehicle.do") {
 //号码
-    new_element(url_No1 + "viewCustomerVehicle.js");
+    new_element(url_No1 + "/viewCustomerVehicle.js");
     
 } else if (url == "/nets-tmr-pac-pad/showSaletHistory.screen") {
 //拨打
-    new_element(url_No1 + "showSaletHistory.js");
+    new_element(url_No1 + "/showSaletHistory.js");
 	
 } else if (url == "/nets-tmr-pac-pad/editCustomerTel.do"){
 //改	
-	new_element(url_No1 + "editCustomerTel.js");
+	new_element(url_No1 + "/editCustomerTel.js");
 	
 } else {
     window.open("http://btp-oms-pac-openresty-prd.paic.com.cn/nets-tmr-pac-pad/viewCustomerVehicle.do", "target");
